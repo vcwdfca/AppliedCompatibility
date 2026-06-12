c697911a07d3b9ae43006d858d8d7ad951252f2a
Circulate233 <a18966009058@163.com>
2026-06-12 17:36:42 +0800

    init

---
+++ src/main/java/github/formlessdragon/appcompat/AppliedCompatibility.java
@@ -0,0 +1,34 @@
+package github.formlessdragon.appcompat;
+
+import github.formlessdragon.appcompat.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
+public class AppliedCompatibility {
+
+    public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
+
+    @SidedProxy(modId = Tags.MOD_ID, clientSide = "github.formlessdragon.appcompat.proxy.ClientProxy", serverSide = "github.formlessdragon.appcompat.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        proxy.preInit(event);
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+}
---
+++ src/main/java/github/formlessdragon/appcompat/proxy/ClientProxy.java
@@ -0,0 +1,24 @@
+package github.formlessdragon.appcompat.proxy;
+
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public class ClientProxy extends CommonProxy {
+
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+    }
+
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+        super.postInit(event);
+    }
+
+}
