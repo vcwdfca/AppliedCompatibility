623eb820ae2a72816a5ba298c6ebc9fc0c29cb92
Circulate233 <a18966009058@163.com>
2026-06-20 22:49:31 +0800

    fix bug

--- src/main/java/appeng/api/storage/IStorageChannel.java
+++ src/main/java/appeng/api/storage/IStorageChannel.java
@@ -3,4 +3,6 @@ package appeng.api.storage;
 import appeng.api.storage.data.IAEStack;
 
 public interface IStorageChannel<T extends IAEStack<T>> {
+
+    T createStack(Object input);
 }
--- src/main/java/com/mekeng/github/common/me/storage/IGasStorageChannel.java
+++ src/main/java/com/mekeng/github/common/me/storage/IGasStorageChannel.java
@@ -2,6 +2,16 @@ package com.mekeng.github.common.me.storage;
 
 import appeng.api.storage.IStorageChannel;
 import com.mekeng.github.common.me.data.IAEGasStack;
+import com.mekeng.github.common.me.data.impl.AEGasStack;
+import mekanism.api.gas.GasStack;
 
 public interface IGasStorageChannel extends IStorageChannel<IAEGasStack> {
+
+    @Override
+    default IAEGasStack createStack(final Object input) {
+        if (input instanceof GasStack stack) {
+            return AEGasStack.of(stack);
+        }
+        return null;
+    }
 }
